67b6bb44cbef8bd8b2f9444978d313d138081f46
HimashaNethmini <himashanethmini05@gmail.com>
2023-03-24 16:49:05 +0530

    Make DB Connection

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+//invoking express
+const app = express();
+
+//declaring a port to run the app
+const PORT = 8000;
+
+//Make DB Connection
+const DB_URL = 'mongodb+srv://himasha:himasha24@cluster0.fpb7ado.mongodb.net/backend?retryWrites=true&w=majority';
+
+mongoose.connect(DB_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology: true
+})
+.then (()=>{
+    console.log("DB Connected");
+})
+.catch((err) => console.log('DB Connection Error',err));
+
+app.listen(PORT,()=>{
+    console.log(`App is running on ${PORT}`);
+});
+
