0be84405bad839d449468f5cff764af9528d5851
HimashaNethmini <himashanethmini05@gmail.com>
2023-03-24 09:54:52 +0530

    Make Rotues

---
+++ routes/posts.js
@@ -0,0 +1,23 @@
+import express from "express";
+import index from "../models/index";
+
+const router = express.Router();
+
+//save 
+router.post('/post/save',(req,res)=>{
+    let newPost = new Posts (req.body);
+
+    newPost.save((err) =>{
+        if(err){
+            return res.status(400).json({
+                error:err
+
+            });
+        }
+        return res.status(200).json({
+            success:"Posts saved successfully"
+        });
+    });
+});
+
+module.exports = router;
