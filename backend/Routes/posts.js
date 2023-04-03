872bd4e3df92620080570ce05fef6535e5298afc
HimashaNethmini <himashanethmini05@gmail.com>
2023-04-03 18:49:13 +0530

    Updated Routes

--- backend/Routes/posts.js
+++ backend/Routes/posts.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Posts = require('../Model/posts');
 const router = express.Router();
 
-//save Posts
+//save
 router.post('/post/save',(req,res) =>{
     let newPost = new Posts (req.body);
 
@@ -17,6 +17,61 @@ router.post('/post/save',(req,res) =>{
             success:"Posts saved successfully"
         });
     });
+
+    //get 
+    router.get('/posts',(req,res) =>{
+        Posts.find().exec((err,posts) =>{
+            if(err){
+                return res.status(400).json({
+                    error:err
+                });
+            }
+            return res.status(200).json({
+                success:true,
+                existingPosts:posts
+            });
+        });
+
+    });
+
+    //update
+    router.get('/post/update/:id',(req,res) =>{
+        Posts.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set:req.body
+            },
+            (err,post)=>{
+                if(err){
+                    return res.status(400).json({
+                        error:err
+                    });
+                }
+                return res.status(200).json({
+                    success:"Updated successfully"
+                });
+            }
+        );
+    });
+
+
+    //delete 
+    router.delete('/post/delete/:id',(req,res) =>{
+        Posts.findByIdAndRemove(req.params.id).exec((err,deletedPost) =>{
+            if(err) {
+                return res.status(400).json({
+                    message:"Deleted unsuccessful",err
+                });
+            }
+            return res.json({
+                message:"Deleted unsuccessful",deletedPost
+            
+            });
+            
+        });
+    });
+
+
 });
 
 module.exports = router;
