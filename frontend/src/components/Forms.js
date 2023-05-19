53f83a4977d4f49caf418916f60b927d13ec2021
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-19 22:37:11 +0530

    Make model

---
+++ frontend/src/components/Forms.js
@@ -0,0 +1,13 @@
+//array of objects
+
+const Forms=[{
+    id:'1003',
+    productname:'panadol',
+    name:'Himasha',
+    mail:'gjhkjkj@gmail.com',
+    contact: '5555222'
+},
+
+
+]
+export default Forms;
