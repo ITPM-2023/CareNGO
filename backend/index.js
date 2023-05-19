6d3307b7e11b572a253fd222bbe525f8c1b71fb4
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-19 21:25:05 +0530

    Updated index.js file

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,81 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const postRoutes = require("./Routes/posts");
-const cors = require('cors');
+//import express
+const express=require('express')
 
-//invoking express
-const app = express();
+//import cors
+const cors=require('cors')
 
-//app middleware
-app.use(bodyParser.json());
+//import logic
+const logic=require('./services/logic')
 
-//route middleware
+//create server
+const server=express()
 
-app.use("/Posts", postRoutes);
-app.use(cors());
-//declaring a port to run the app
-const PORT = 8000;
+//connections
 
-//Make DB Connection
-const DB_URL = 'mongodb+srv://himasha:himasha24@new.3hgpv5f.mongodb.net/?retryWrites=true&w=majority';
+server.use(cors({
+    origin:'http://localhost:3000'
+}))
 
-mongoose.connect(DB_URL,{
-    useNewUrlParser : true,
-    useUnifiedTopology: true
+server.use(express.json());
+
+server.listen(8000,()=>{
+    console.log('listening on port 8000');
+    console.log('DB Connected');
 })
-.then (()=>{
-    console.log("DB Connected");
+
+//getting all forms details
+
+server.get('/allforms',(req,res)=>{
+    logic.allForms().then(
+        (result)=>{
+            res.status(result.statusCode).json(result)
+        }
+        
+    )
 })
-.catch((err) => console.log('DB Connection Error',err));
 
-app.listen(PORT,()=>{
-    console.log(`App is running on ${PORT}`);
-});
+//add form details
 
+server.post('/addforms',(req,res)=>{
+    logic.addForms(req.body.id, req.body.productname, req.body.box, req.body.name, req.body.mail, req.body.contact).then(
+        (result)=>{
+            res.status(result.statusCode).json(result)
+        }
+        
+    )
+})
+
+
+//edit form details
+
+server.post('/editforms',(req,res)=>{
+    logic.editForms(req.body.id, req.body.productname, req.body.box, req.body.name, req.body.mail, req.body.contact).then(
+        (result)=>{
+            res.status(result.statusCode).json(result)
+        }
+        
+    )
+})
+
+//delete form details
+
+server.delete('/deleteforms/:id',(req,res)=>{
+    logic.deleteForms(req.params.id).then(
+        (result)=>{
+            res.status(result.statusCode).json(result)
+        }
+        
+    )
+})
+
+//get form details
+
+server.get('/getforms/:id',(req,res)=>{
+    console.log(req.params.id);
+    logic.getForms(req.params.id).then(
+        (result)=>{
+            res.status(result.statusCode).json(result)
+        }
+        
+    )
+})
